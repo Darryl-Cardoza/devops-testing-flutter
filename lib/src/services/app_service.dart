bfb7f30a3028c5d9532a17b654f4a8ef53d335fb
Darryl Cardoza <darryl.cardoz@aviradigital.com>
2025-04-09 14:20:05 +0530

    Added env file

--- lib/src/services/app_service.dart
+++ lib/src/services/app_service.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_movie_deep_dive_test/src/models/models.dart';
 import 'package:http/http.dart' show Client;
 
 class LoadMoviesException implements Exception {
-  final dynamic message;
+  final message;
 
   LoadMoviesException(this.message);
 }
@@ -16,17 +15,19 @@ class AppService {
   AppService(this.client);
 
   Future<MoviesResponse> loadMovies() async {
-    final apiKey = dotenv.env['API_KEY'] ?? '';
-    final api = dotenv.env['API_NAME'] ?? '';
-    final urlPath = dotenv.env['URL_PATH'] ?? '';
+    final apiKey = '4205ec1d93b1e3465f636f0956a98c64';
+    final api = 'https://api.themoviedb.org/3';
+    final urlPath = 'movie/now_playing';
     final path = '$api/$urlPath?api_key=$apiKey&language=en-US';
 
+    // appel asynchrone
     final response = await client.get(Uri.parse(path));
 
     if (response.statusCode != 200) {
       throw LoadMoviesException('LoadMovies - Request Error: ${response.statusCode}');
     }
 
+    // Décoder le contenu de la response ici
     final data = json.decode(response.body);
 
     return MoviesResponse.fromJson(data);
